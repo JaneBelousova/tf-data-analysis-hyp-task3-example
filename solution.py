53e95f7bf3f974d515530073f57b8b912313529c
JaneBelousova <42033480+JaneBelousova@users.noreply.github.com>
2023-04-14 01:59:17 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import kruskal
+from scipy.stats import mannwhitneyu
 
 chat_id = 1124136722 # Ваш chat ID, не меняйте название переменной
 
@@ -9,5 +9,7 @@ def solution(x: np.array, y: np.array) -> bool: # Одна или две выб�
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     alfa = 0.07
-    _, p = kruskal(x, y)
+    x_list = [i for i in x]
+    y_list = [k for k in y]
+    p = mannwhitneyu(x_list, y_list, alternative="greater").pvalue
     return p < alfa # Ваш ответ, True (отклонить Но, конверсия уменьшилась) или False (не отклонять Но, конверсия не изменилась)
